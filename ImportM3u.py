01a115d0587c0469059d40cd49cdb4b93dc0e215
zackramjan <zack.hr@gmail.com>
2019-05-29 01:36:37 -0400

    added prefix support and pkg support

--- ImportM3u.py
+++ ImportM3u.py
@@ -5,16 +5,18 @@ Created on May 27, 2019
 '''
 
 import sys
-import os
-import mysql.connector
-
 import M3uParser
 import MinistraSQL
 
 def main(argv=None): 
+    
+    
+    
+    
+    
     myM3u = M3uParser.M3uParser();
     myM3u.readM3u(sys.argv[1])
-    sql = MinistraSQL.MinistraSQL("username","password","localhost") 
+    sql = MinistraSQL.MinistraSQL("username","password","localhost",argv[2]) 
     for i in myM3u.getList():
         sys.stderr.write(i["title"] + " ^ " + i["tvg-name"] + " ^ " + i["tvg-ID"] + " ^ " + i["tvg-group"] + " ^ " + i["link"] + "\n")
         sql.insertChannel(i)
--- MinistraSQL.py
+++ MinistraSQL.py
@@ -12,11 +12,11 @@ class MinistraSQL(object):
     classdocs
     '''
 
-    def __init__(self, username, password, dbhost):
+    def __init__(self, username, password, dbhost, prefixIn):
         '''
         Constructor
         '''
-        
+        self.prefix = prefixIn
         self.myCon = mysql.connector.connect(
               host=dbhost,
               user=username,
@@ -25,12 +25,15 @@ class MinistraSQL(object):
             )
 
     def insertChannel(self, item):
+        #insert/create the channels group as a genre and pkg 
         self.checkInsertGenre(item["tvg-group"])
         gid = self.getGenreID(item["tvg-group"])
+        pid = self.checkInsertPkg(item["tvg-group"])
         maxCh = self.getMaxChannel()
         
+        #add teh channel
         query = "INSERT  INTO itv (name,number,cmd,base_ch,tv_genre_id,xmltv_id) VALUES( %s, %s, %s, 1, %s, %s)"
-        values = (item["tvg-name"], maxCh, item["link"], gid, item["tvg-ID"])
+        values = (item["tvg-name"], maxCh, item["link"], gid, self.prefix + item["tvg-ID"])
         cursor = self.myCon.cursor()
         cursor.execute(query, values)
         chId = cursor.lastrowid
@@ -41,6 +44,15 @@ class MinistraSQL(object):
         cursor = self.myCon.cursor()
         cursor.execute(query, values)
         self.myCon.commit()
+        
+        #insert the channel in the pkg
+        query = "INSERT  INTO service_in_package (service_id,package_id,type) VALUES( %s, %s,%s)"
+        values = (chId,pid,"tv")
+        cursor = self.myCon.cursor()
+        cursor.execute(query, values)
+        self.myCon.commit()
+        
+        
 
     def checkInsertGenre(self,genre):
         maxGen = self.getMaxGenre()
@@ -50,6 +62,19 @@ class MinistraSQL(object):
         cursor.execute(query, values)
         self.myCon.commit()
         
+    def checkInsertPkg(self,genre):
+        pkgID = self.getPkgID(genre)
+        if pkgID > 0:
+            return pkgID
+        
+        query = "INSERT INTO services_package (external_id,name,type) VALUES (%s,%s,%s)"
+        values = (genre,genre,"tv")
+        cursor = self.myCon.cursor()
+        cursor.execute(query, values)
+        pkgID = cursor.lastrowid
+        self.myCon.commit()    
+        return pkgID
+        
     def getGenreID(self,genre):
         query = "select id from tv_genre where title = %s"
         values = (genre,)
@@ -58,6 +83,17 @@ class MinistraSQL(object):
         res = cursor.fetchone()   
         return res[0]
     
+    def getPkgID(self,genre):
+        query = "select id from services_package where name = %s"
+        values = (genre,)
+        cursor = self.myCon.cursor()
+        cursor.execute(query, values)
+        if (cursor.rowcount() > 0):
+            res = cursor.fetchone()   
+            return res[0]
+        else :
+            return -1
+        
     
     def getMaxChannel(self):
         query = "select max(number) as max from itv"
@@ -73,3 +109,19 @@ class MinistraSQL(object):
         res = cursor.fetchone()   
         return res[0] + 1;
     
+    def DeleteAllChannels(self):
+        query = "delete from itv where id > 0"
+        cursor = self.myCon.cursor()
+        cursor.execute(query)
+        self.myCon.commit()
+        
+        query = "delete from ch_links where id > 0"
+        cursor = self.myCon.cursor()
+        cursor.execute(query)
+        self.myCon.commit()
+    
+    def DeleteAllGenre(self):
+        query = "delete from tv_genre where id > 0"
+        cursor = self.myCon.cursor()
+        cursor.execute(query)
+        self.myCon.commit()
