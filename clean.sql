3f1b07c5e8fed9375f748c8fcce4de7087e1d28d
zackramjan <zack.hr@gmail.com>
2019-06-01 07:11:35 +0000

    added clean sql

---
+++ clean.sql
@@ -0,0 +1,8 @@
+delete from itv;
+delete from ch_links;
+delete from tariff_plan;
+delete from package_in_plan;
+delete from user_package_subscription;
+delete from services_package;
+delete from service_in_package;
+delete from tv_genre;
