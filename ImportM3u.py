b6d40d43fdc216e8b7b4162913bfafde7d1aad7f
zackramjan <zack.hr@gmail.com>
2019-06-11 00:50:03 -0400

    fixed bugs

--- ImportM3u.py
+++ ImportM3u.py
@@ -11,41 +11,42 @@ import MinistraSQL
 import os
 import argparse
 
-def useGenreMapFile(self,GenreMapFile):
-    self.genreMap = {}
-    with open(GenreMapFile) as f:
-        for line in f:
-            if ":" in line:
-                (key, val) = line.split(":")
-                self.genreMap[key.rstrip()] = val.rstrip()
-
 def main(argv=None): 
     parser = argparse.ArgumentParser(description='Process import args.')
     parser.add_argument('-t', '--tag', help='xmltv-id prefix: any tag/id of your choice. channels will be added to a tariff with this name',default="")
     parser.add_argument('-g', '--genre', help='genre mapping text file name in format: XMLTV-Group name:your genre  entries that end in ^ will cause that groups channels to be skipped')
     parser.add_argument('-n', '--channel', help='A prefix for your channel names', default="")
-    parser.add_argument('-r', '--remove', help='clear out existing channels first',action='store_true')
+    parser.add_argument('-r', '--remove', help='clear out existing channels/movies first',action='store_true')
     requiredNamed = parser.add_argument_group('required arguments')
     requiredNamed.add_argument('-m', '--m3u', help='Input m3u name', required=True)
     args=parser.parse_args()
     
     sql = MinistraSQL.MinistraSQL("root","st@lk3r","localhost",args.tag)
     
+    #remove channels if flag set
     if args.remove:
         sql.cleanChannels()
-        
-    myM3u = M3uParser.M3uParser();
-    myM3u.readM3u(args.m3u)
+        sql.cleanMovies()
     
+    #read in the genreMap file
     genreMap = {} 
     if os.path.isfile(args.genre):
-        useGenreMapFile(args.genre)
-     
+        with open(args.genre) as f:
+            for line in f:
+                if ":" in line:
+                    (key, val) = line.split(":")
+                    genreMap[key.rstrip()] = val.rstrip()
+    
+    #process the m3u and insert
+    myM3u = M3uParser.M3uParser();
+    myM3u.readM3u(args.m3u) 
+    
     for i in myM3u.getList():
         genre = i["tvg-group"]
         if i["tvg-group"] in genreMap.keys():
             genre = genreMap[i["tvg-group"]]     
         
+        #skip if genre ends with ^
         if genre.endswith("^"):
             continue
         
